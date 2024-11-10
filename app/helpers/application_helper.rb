f9a394fd3adedf5706427d376245c90660a9984f
thomis <thomas.steiner@ikey.ch>
2024-11-10 23:08:28 +0100

    Have a navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def get_header_classes(controller)
+    case "#{controller.controller_name}##{controller.action_name}"
+    when "events#show"
+      "bg-slate-700"
+    else
+      "bg-slate-500/30 backdrop-blur-lg"
+    end
+  end
 end
